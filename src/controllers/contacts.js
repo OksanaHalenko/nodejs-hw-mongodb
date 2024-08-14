94a1007e873cca634c3e3ca9ace603fbb4a98ea1
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-08-14 12:10:31 +0200

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -23,7 +23,7 @@ export const getContactByIdController = async (req, res, next) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    next(createError(404, 'Student not found'));
+    next(createError(404, 'Contact not found'));
   }
   res.status(200).json({
     status: 200,
