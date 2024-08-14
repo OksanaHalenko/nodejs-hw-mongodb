0cbed4bdce2f46123b38b67d10b36196150072d0
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-08-14 12:12:09 +0200

    Update notFoundHandler.js

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,10 +1,5 @@
 import createError from 'http-errors';
 
 export const notFoundHandler = (req, res, next) => {
-  throw createError(
-    res.status(404).json({
-      status: 404,
-      message: 'Route not found',
-    }),
-  );
+  next(createError(404, 'Route not found'));
 };
