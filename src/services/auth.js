1908e464c2812844f5861c426dd1859107b81280
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-08-18 10:24:16 +0200

    add bcrypt for password

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,17 @@
+import bcrypt from 'bcrypt';
+
 import { UsersCollection } from '../db/models/user.js';
+import createError from 'http-errors';
 
 export const registerUser = async (payload) => {
-  return await UsersCollection.create(payload);
+  const user = await UsersCollection.findOne({ email: payload.email });
+
+  if (user) throw createError(409, 'Email in use');
+
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  return await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
 };
