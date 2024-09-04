7cf49554ecc211f18423990265d0d14f6762f67e
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-09-04 12:43:19 +0200

    add router for swagger

---
+++ src/middlewares/swaggerDocs.js
@@ -0,0 +1,15 @@
+import createError from 'http-errors';
+import swaggerUI from 'swagger-ui-express';
+import fs from 'node:fs';
+
+import { SWAGGER_PATH } from '../constants/index.js';
+
+export const swaggerDocs = () => {
+  try {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+  } catch (err) {
+    return (req, res, next) =>
+      next(createError(500, "Can't load swagger docs"));
+  }
+};
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { env } from './utils/env.js';
 import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { TEMP_UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(env('PORT', '3000'));
@@ -32,6 +33,7 @@ export const setupServer = () => {
     }),
   );
   app.use('/uploads', express.static(TEMP_UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(router);
 
   app.use('*', notFoundHandler);
